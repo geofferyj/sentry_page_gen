612fd7ddc6eda0ef7b45bc583ec16922e328f196
geofferyj <geofferyjoseph1@gmail.com>
2020-06-11 14:45:32 +0100

    finishing touches add

--- project/site_pages/urls.py
+++ project/site_pages/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from site_pages.views import AddPageView, ListPageView, UpdatePageView, PageContentView
 
 urlpatterns = [
-    path('',ListPageView.as_view() ),
-    path('add-page/',AddPageView.as_view() ),
-    path('set-page-markdown/<int:pk>/',UpdatePageView.as_view() ),   
-    path('get-page-html/<int:pk>/',PageContentView.as_view() ),  
+    path('list_pages/',ListPageView.as_view() ),
+    path('add_page/',AddPageView.as_view() ),
+    path('set_page_markdown/<slug:slug>/',UpdatePageView.as_view() ),   
+    path('retrieve_page_html/<slug:slug>/',PageContentView.as_view() ),  
 
 ] 
